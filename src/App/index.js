0ba6f7a5ac2ff2d6802051b17a36e98c70d5df43
Mike Schutte <schutte@quikly.com>
2017-12-14 08:28:37 -0500

    Remove CRA boilerplate, setup new dir structure

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+          <h1>Welcome to Mogonono</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
