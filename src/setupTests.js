1ef78574ce17f04de82086c880ee9740eabcbb53
Mike Schutte <schutte@quikly.com>
2017-12-24 23:38:48 -0700

    Updates

--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import "jest-enzyme";
-import { configure, shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-configure({ adapter: new Adapter() });
-
-global.shallow = shallow;
